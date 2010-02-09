23fa16beb353f39c246c37830a4d16e1af3a51f8
Johnny Preyer <jpreyer@jp-ubuntu.(none)>
2010-02-09 05:07:18 -0500

    adding new structure for python code added comment to p.py

--- p.py
+++ p.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+# testing the range function in python
 numbers=range(1,17)
 t=4
 numbers.remove(t)
---
+++ python/problem_12.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python
+# Template to use for python programs
