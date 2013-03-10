fd644d9cbb113a84600fb4a297ab46efb2ab495d
jpreyer <jpreyer@gmail.com>
2013-03-09 23:37:58 -0500

    removing debug statements and comments

--- problem13/problem13.py
+++ problem13/problem13.py
@@ -18,23 +18,14 @@ while j >= 0:
 	#print j
 	i = 0
 	while i<100:
-#		print "i : ", i
-#		print b[i][49]
 		total += int(b[i][j])
 		i += 1
 	j = j - 1
 	digit = total%10
-	#print digit
 	answer.append(str(digit))
 	total = (total-digit)/10
 
 
 answer.append(str(total))
 answer.reverse()
-#print "total: ", total
-#print "digit: ", digit
 print ''.join(answer)
-#for ii in answer:
-#	print ii,
-#	print
-#print answer
