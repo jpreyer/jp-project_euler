d44700b90ff88687be9d0dc28c9e5e1465c35f4b
Johnny Preyer <jpreyer@gmail.com>
2014-05-18 22:22:48 -0400

    Finally finished with problem 21

--- problem21/problem21.py
+++ problem21/problem21.py
@@ -1,5 +1,7 @@
 import math
 n=4
+A={}
+B={}
 while n <= 10000:
     L=[1]
     sum=0
@@ -7,10 +9,22 @@ while n <= 10000:
     for i in xrange (2,k):
         if (n % i) == 0:
             L.append(i)
-            L.append(n / i)
+            t = n/i
+            if (t != i):
+                L.append(t)
 
     for j in L:
         sum += j
 
-    print "d(",n,") = ", sum, "  ", sorted(L)
+    A[n] = sum
     n += 1
+
+sum1=0
+for ii in A.keys():
+    s1 = A[ii]
+    if s1 > 3 and s1 < 10000:
+        if A[s1] == ii and A[ii] == s1 and ii != A[ii]:
+            sum1 += ii
+            print ii, "  ", A[ii]
+
+print sum1
